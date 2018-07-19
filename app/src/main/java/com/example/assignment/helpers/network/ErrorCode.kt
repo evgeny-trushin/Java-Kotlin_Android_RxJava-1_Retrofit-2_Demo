b81fd8f5725c3b2956c9788194096dc6e993f099
Evgeny Trushin <Evgeny.Trushin@Inteliment.com>
2018-07-19 20:07:43 +1000

    [WIP] Backup

---
+++ app/src/main/java/com/example/assignment/helpers/network/ErrorCode.kt
@@ -0,0 +1,18 @@
+package com.example.assignment.helpers.network
+
+import com.example.assignment.helpers.AppApplication
+import com.example.assignment.i002.R
+
+enum class ErrorCode(private val errorCode: Int) {
+    ERROR_NETWORK_UNAVAILABLE(R.string.error_network_unavailable),
+    ERROR_HOST_UNAVAILABLE(R.string.error_host_unavailable),
+    ERROR_HOST_FAULT(R.string.error_host_fault),
+    ERROR_HOST_TIMEOUT(R.string.error_host_fault),
+    ERROR_RESPONSE_DATA_ARE_NULL(R.string.error_host_fault),
+    ERROR_RESPONSE_CORRUPTED_DATA(R.string.error_response_corrupted_data),
+    ERROR_INTERNAL(R.string.error_internal),
+    ERROR_IGNORE(R.string.error_ignor);
+
+    fun getDescriptionOfError() = AppApplication.getAppContext().getString(this.errorCode)!!
+
+}
